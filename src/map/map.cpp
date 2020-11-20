262834b3cafaa363ecb51c18537387e3c24bb6ac
Martin <sederholm.martin@gmail.com>
2020-11-20 17:23:13 +0200

    Add map with Draw function

---
+++ src/map/map.cpp
@@ -0,0 +1,33 @@
+#include "map.hpp"
+
+Map::Map(int height, int width) 
+    : height_(height), width_(width) {     
+    for (int i = 0; i < height; i++) {
+        tiles_.push_back(std::vector<Tile*>());
+        for (int j = 0; j < width; j++) {
+            tiles_[i].push_back(new Tile((TileTypes)(rand() % 3)));
+        }
+    }
+}
+
+Tile* Map::GetTile(int x, int y) const {
+    return tiles_[y][x];
+}
+
+void Map::Draw(sf::RenderWindow& window) {
+  auto windowsize = window.getSize();
+  int tile_size_x = (windowsize.x - 200) / width_;
+  int tile_size_y = (windowsize.y - 200) / height_;
+  auto tile_size = std::min(tile_size_x, tile_size_y);
+  for (int y = 0; y < height_; y++) {
+    for (int x = 0; x < width_; x++) {
+      Tile* tile = tiles_[y][x];
+      tile->SetPos(x * tile_size, y * tile_size);
+      tile->SetScale(tile_size);
+      tile->draw(window);
+    }
+  }
+}
+
+
+
---
+++ src/map/map.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <../SFML/Graphics.hpp>
+#include <vector>
+
+#include "tile.hpp"
+
+class Map {
+public:
+    Map(int height = 8, int width = 12);
+    ~Map() { };
+    Tile* GetTile(int x, int y) const;
+    void Draw(sf::RenderWindow& window);
+private:
+    int height_;
+    int width_;
+    std::vector<std::vector<Tile*>> tiles_; // _tiles[_height][_width]
+};
