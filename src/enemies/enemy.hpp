6764170efc2e103353854d7522af19d6eb1c6a76
Martin <sederholm.martin@gmail.com>
2020-11-27 15:48:43 +0200

    Start work on enemies

---
+++ src/enemies/enemy.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+enum TypesOfEnemies {
+    Normal,
+    Multiple,
+    Spawn,
+    Air
+};
+
+class Enemy {
+public:
+    Enemy(float max_hp, float x, float y, float speed, float delay, int size,
+    TypesOfEnemies type = Normal);
+
+private:
+    float               hp_;
+    const float         max_hp_;
+    float               x_cord_;
+    float               y_cord_;
+    float               speed_;
+    float               delay_;
+    int                 size_;
+    TypesOfEnemies      type_;
+};
+
