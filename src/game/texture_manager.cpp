090cf8e1d343c43f284ab6a565197bb0457d1e6f
Martin <sederholm.martin@gmail.com>
2020-11-29 00:41:52 +0200

    Format TextureManager

--- src/game/texture_manager.cpp
+++ src/game/texture_manager.cpp
@@ -1,11 +1,11 @@
 #include "texture_manager.hpp"
 
-Texture_Manager& Texture_Manager::GetManager() {
-    static Texture_Manager manager;
+TextureManager& TextureManager::GetManager() {
+    static TextureManager manager;
     return manager;
 }
 
-sf::Texture& Texture_Manager::GetTexture(const std::string& path) {
+sf::Texture& TextureManager::GetTexture(const std::string& path) {
     auto texture = textures_.find(path);
     if(texture == textures_.end()) {
         texture = LoadTexture(path);
@@ -13,7 +13,7 @@ sf::Texture& Texture_Manager::GetTexture(const std::string& path) {
     return *texture->second;
 }
 
-std::map<const std::string, std::unique_ptr<sf::Texture>>::iterator Texture_Manager::LoadTexture(const std::string& path) {
+texture_map::iterator TextureManager::LoadTexture(const std::string& path) {
     std::unique_ptr<sf::Texture> texture = std::make_unique<sf::Texture>(sf::Texture());
     texture->loadFromFile(path);
     return textures_.insert(std::make_pair(path, std::move(texture))).first;
--- src/game/texture_manager.hpp
+++ src/game/texture_manager.hpp
@@ -2,14 +2,16 @@
 
 #include <SFML/Graphics.hpp>
 
-class Texture_Manager {
+typedef std::map<std::string, std::unique_ptr<sf::Texture>> texture_map;
+
+class TextureManager {
 public:
-    static Texture_Manager& GetManager();
-    sf::Texture& GetTexture(const std::string& path);
-    std::map<const std::string, std::unique_ptr<sf::Texture>>::iterator LoadTexture(const std::string& path);
+    static TextureManager& GetManager();
+    sf::Texture&        GetTexture(const std::string& path);
+    texture_map::iterator LoadTexture(const std::string& path);
 
 private:
-    std::map<std::string, std::unique_ptr<sf::Texture>> textures_;
+    texture_map         textures_;
 };
 
-#define texture_manager Texture_Manager::GetManager()
+#define texture_manager TextureManager::GetManager()
