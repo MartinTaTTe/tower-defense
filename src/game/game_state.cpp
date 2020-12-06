079dc35867cdc5d4926a260ea51aa6e8661db25d
mesterc1 <carl.mesterton@aalto.fi>
2020-12-06 13:24:46 +0200

    button grafik

--- src/game/game_state.cpp
+++ src/game/game_state.cpp
@@ -9,7 +9,7 @@ GameState::GameState(int width, int height, const std::string& mapPath)
     : State("Game State", width, height) {
     AddCanvas({0, 0, 0.8f, 1}, mapPath); // map
     AddCanvas({0.8f, 0, 1, 1}); // siderbar
-    canvases_.back().second->AddButton({0, 0.8f, 1, 1}, T_RETURN_TO_MENU, Event(EventType::PopState)); // return to menu
+    canvases_.back().second->AddButton({0, 0.8f, 1, 1}, T_RETURN_TO_MENU_BUTTON, Event(EventType::PopState)); // return to menu
     canvases_[0].second->AddText({0, 0}, "0 FPS", 30); // FPS counter
 }
 
--- src/game/input_state.cpp
+++ src/game/input_state.cpp
@@ -5,17 +5,17 @@ InputState::InputState(int width, int height)
     AddCanvas({0.2f, 0.2f, 0.8f, 0.8f}); // main interface
     Event event(EventType::ChangeMapSize);
     event.increments = {1, 0};
-    canvases_.back().second->AddButton({0, 0, 0.45f, 0.2f}, T_DEFAULT_BUTTON, event); // increment x
+    canvases_.back().second->AddButton({0, 0, 0.45f, 0.2f}, T_PLUS_BUTTON, event); // increment x
     event.increments = {0, 1};
-    canvases_.back().second->AddButton({0.55f, 0, 1, 0.2f}, T_DEFAULT_BUTTON, event); // decrement x
+    canvases_.back().second->AddButton({0.55f, 0, 1, 0.2f}, T_MINUS_BUTTON, event); // decrement x
     event.increments = {-1, 0};
-    canvases_.back().second->AddButton({0, 0.5f, 0.45f, 0.7f}, T_DEFAULT_BUTTON, event); // increment y
+    canvases_.back().second->AddButton({0, 0.5f, 0.45f, 0.7f}, T_PLUS_BUTTON, event); // increment y
     event.increments = {0, -1};
-    canvases_.back().second->AddButton({0.55f, 0.5f, 1, 0.7f}, T_DEFAULT_BUTTON, event); // decrement y
+    canvases_.back().second->AddButton({0.55f, 0.5f, 1, 0.7f}, T_MINUS_BUTTON, event); // decrement y
     canvases_.back().second->AddText({0.2f, 0.3f}, "2", 50); // x
     canvases_.back().second->AddText({0.7f, 0.3f}, "2", 50); // y
     AddCanvas({0.8f, 0, 1, 1}); // sidebar
-    canvases_.back().second->AddButton({0, 0.8f, 1, 1}, T_RETURN_TO_MENU, Event(EventType::PopState)); // return to menu
+    canvases_.back().second->AddButton({0, 0.8f, 1, 1}, T_RETURN_TO_MENU_BUTTON, Event(EventType::PopState)); // return to menu
     canvases_.back().second->AddButton({0, 0, 1, 0.2f}, T_DEFAULT_BUTTON, Event(EventType::PushMapEditorState));
 }
 
